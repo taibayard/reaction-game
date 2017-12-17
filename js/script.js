1312772151880b86fb5442886dafb2f35e0402c8
taibayard <33849433+taibayard@users.noreply.github.com>
2017-12-17 14:49:19 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,48 @@
+let homeScreen = document.getElementById("home-screen");
 let playBtn = document.getElementById("play-btn");
 let homeLabel = document.getElementsByClassName("title")[0];
+let stopLight = document.getElementById("stop-light");
+let lights = document.getElementsByClassName("light");
+let main = document.getElementsByTagName("main")[0];
+let currentLight = 0;
+var randomTime = function(min, max) {
+    return ((Math.random() * max) + min)*1000;
+}
+
+function changeLights() {
+    let timer = randomTime(0, 2);
+    console.log(timer);
+    setTimeout(function() {
+        switch (true) {
+            case currentLight === 0:
+                lights[currentLight].style.backgroundColor = "red";
+                changeLights();
+                break;
+            case currentLight === 1:
+                lights[currentLight].style.backgroundColor = "yellow";
+                changeLights();
+                break;
+            case currentLight === 2:
+                lights[currentLight].style.backgroundColor = "green";
+                changeLights();
+                break;
+            case currentLight > 2:
+            	stopLight.style.display = "none";
+            break;
+        }
+        currentLight++;
+    }, timer)
+}
+
+function playBtnClick() {
+    homeScreen.style.setProperty("display", "none");
+    main.style.setProperty("display", "block");
+    changeLights();
+}
+
+function addListeners() {
+    playBtn.addEventListener("click", playBtnClick);
+}
+window.onload = function() {
+    addListeners();
+}
